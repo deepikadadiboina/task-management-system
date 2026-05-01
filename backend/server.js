6664e8fe532c8331b18e603ed5367dacb3978d7d
deepika dadiboina <udaycharn68@gmail.com>
2026-05-01 12:02:47 +0530

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(cors());
 
 // DB
-mongoose.connect("mongodb://127.0.0.1:27017/taskmanager")
+mongoose.connect(process.env.MONGO_URI || "mongodb://127.0.0.1:27017/taskmanager")
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
@@ -20,4 +20,5 @@ app.get("/", (req, res) => {
   res.send("API Running");
 });
 
-app.listen(5000, () => console.log("Server running on 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
